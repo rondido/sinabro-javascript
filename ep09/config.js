5821fd69b24fceae4fe74db76dd6003af1c2e6d9
rondido <rppr001@naver.com>
2023-11-17 21:43:20 +0900

    정적 블로그 만들기 완료

--- ep09/config.js
+++ ep09/config.js
@@ -3,6 +3,12 @@ export default {
     contents: "contents",
     pages: "pages",
     dist: "dist",
+    contentsSlug: "post",
+    assets: "assets",
   },
-  site: { title: "My Blog!!!!!" },
+  site: { title: "My Blog!!!!!", author: "Jinhyeon" },
+  updatePost(post) {
+    post.created_at = post.created_at.toLocaleDateString();
+    return post;
+  },
 };
--- ep09/scripts/build.js
+++ ep09/scripts/build.js
@@ -1,18 +1,22 @@
 import fs from "fs-extra";
 import Mustache from "mustache";
+import frontMatter from "front-matter";
 import config from "../config.js";
+import Showdown from "showdown";
 
 const DIST = config.build.dist;
 const PAGES = config.build.pages;
-
+const CONTENTS = config.build.contents;
+const CONTENTS_SLUG = config.build.contentsSlug;
+const ASSETS = config.build.assets;
 async function renderFile(source, dest) {
+  const recentPosts = await getRecentPosts();
   const file = await fs.readFile(source);
-  const result = Mustache.render(file.toString(), config);
+  const result = Mustache.render(file.toString(), { ...config, recentPosts });
   await fs.writeFile(dest, result);
 }
 
-async function build() {
-  await fs.mkdir(DIST);
+async function buildHtmlFiles() {
   const files = await fs.readdir(PAGES);
   for (const file of files) {
     if (file === "index.html") {
@@ -23,6 +27,54 @@ async function build() {
       await renderFile(`${PAGES}/${file}`, `${DIST}/${folderName}/index.html`);
     }
   }
+}
+
+async function getRecentPosts() {
+  const files = await fs.readdir(CONTENTS);
+  const result = [];
+  for (const file of files) {
+    const { attributes } = frontMatter(
+      (await fs.readFile(`${CONTENTS}/${file}/index.md`)).toString()
+    );
+    result.push({
+      ...attributes,
+      path: `/${CONTENTS_SLUG}/${attributes.slug}`,
+    });
+  }
+  return result;
+}
+
+async function buildContentsFiles() {
+  const files = await fs.readdir(CONTENTS);
+  await fs.mkdir(`${DIST}/${CONTENTS_SLUG}`);
+
+  for (const file of files) {
+    const { attributes, body } = frontMatter(
+      (await fs.readFile(`${CONTENTS}/${file}/index.md`)).toString()
+    );
+    const template = await fs.readFile("./templates/post.html");
+    const bodyHtml = new Showdown.Converter().makeHtml(body);
+    const html = Mustache.render(template.toString(), {
+      ...config,
+      post: config.updatePost({ ...attributes, body: bodyHtml }),
+    });
+    await fs.mkdir(`${DIST}/${CONTENTS_SLUG}/${file}`);
+    await fs.writeFile(`${DIST}/${CONTENTS_SLUG}/${file}/index.html`, html);
+  }
+}
+
+async function copyAssets() {
+  const files = await fs.readdir(ASSETS);
+  for (const file of files) {
+    await fs.copy(`${ASSETS}/${file}`, `${DIST}/${file}`);
+  }
+}
+
+async function build() {
+  await fs.mkdir(DIST);
+  await copyAssets();
+  await buildHtmlFiles();
+  await buildContentsFiles();
 }
 
 build();
